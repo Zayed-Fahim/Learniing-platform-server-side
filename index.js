871139fd879bab32360ee2895f6c7fd58fd8cc7f
Zayed Fahim <zayedfahim33.cse@gmail>
2022-10-27 02:57:30 +0600

    that's all for today

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const courses = require('./data/courses.json');
+const course = require('./data/courses.json')
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('./courses/:id',(req, res)=> {
+    const id = req.params.id;
+    console.log(id)
+    const desireCourse = course.find(n => n.category_id === id);
+    res.send(desireCourse)
+})
 
 app.get('/', (req, res) => {
     res.send('learning platform server')
